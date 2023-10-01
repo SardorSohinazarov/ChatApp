ae81254f5d9c72546acb2598200bbefa34ed65a1
user <user@WIN-0GEV2J052PH>
2023-10-02 00:17:01 +0500

    unnecessary folders deleted

--- Chat.WPF/Models/Message.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chat.WPF.Models
-{
-    public class Message
-    {
-        public int Id { get; set; }
-        public string SenderUserName { get; set; }
-        public string Text { get; set; }
-    }
-}
