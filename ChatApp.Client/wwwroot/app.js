9f2076984d3ea22455240f636b84ae54aac1bebb
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-10-14 08:51:37 +0500

    Messages pageni pastiga tushadigan qilib qo'ydik

--- ChatApp.Client/wwwroot/app.js
+++ ChatApp.Client/wwwroot/app.js
@@ -5,3 +5,11 @@ window.getFromStorage = (key) => storage.getItem(key);
 window.setToStorage = (key, value) => storage.setItem(key, value);
 
 window.removeFromStorage = (key) => storage.removeItem(key);
+
+
+function scrollFunction() {
+    const item = document.querySelector(".items-container")
+    item.scrollTop = item.scrollHeight - item.clientHeight
+    console.log("run is js code")
+    alert("ishladi")
+}
